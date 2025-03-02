145f287239f3b931f38fe62b1f3d72d80727894b
bvolovat <borisv@armosec.io>
2025-03-02 09:55:08 +0200

    add config log to git
    Signed-off-by: bvolovat <borisv@armosec.io>

--- performance.py
+++ performance.py
@@ -34,7 +34,7 @@ def setup_logging(output_dir):
     )
     return logging.getLogger()
 
-logger = setup_logging(os.getenv('OUTPUT_DIR', 'config'))     
+logger = setup_logging(os.getenv('OUTPUT_DIR', '/workspace'))     
         
 def log_and_print(message):
     print(message)  # Console
